8a387e524a9afa4ab9cb9fa2f244660572b460b8
Karl Keiser <karl.keiser@k2informatics.ch>
2019-07-16 12:02:39 +0200

    WIP

--- test/oranif_eunit.erl
+++ test/oranif_eunit.erl
@@ -2599,7 +2599,7 @@ load_test() ->
     ok.
 
 
-unsafe_no_context_test_() ->
+unsafe_no_context_test_a() ->
     {
         setup,
         fun() -> setup(#{safe => false}) end,
@@ -2607,7 +2607,7 @@ unsafe_no_context_test_() ->
         ?W(?NO_CONTEXT_TESTS)
     }.
 
-unsafe_context_test_() ->
+unsafe_context_test_a() ->
     {
         setup,
         fun() -> setup_context(#{safe => false}) end,
@@ -2615,7 +2615,7 @@ unsafe_context_test_() ->
         ?W(?AFTER_CONTEXT_TESTS)
     }.
 
-unsafe_session_test_() ->
+unsafe_session_test_a() ->
     {
         setup,
         fun() -> setup_connecion(#{safe => false}) end,
@@ -2623,7 +2623,7 @@ unsafe_session_test_() ->
         ?W(?AFTER_CONNECTION_TESTS)
     }.
 
-no_context_test_() ->
+no_context_test_a() ->
     {
         setup,
         fun() -> setup(#{safe => true}) end,
@@ -2631,7 +2631,7 @@ no_context_test_() ->
         ?W(?NO_CONTEXT_TESTS)
     }.
 
-context_test_() ->
+context_test_a() ->
     {
         setup,
         fun() -> setup_context(#{safe => true}) end,
@@ -2639,7 +2639,7 @@ context_test_() ->
         ?W(?AFTER_CONTEXT_TESTS)
     }.
 
-session_test_() ->
+session_test_a() ->
     {
         setup,
         fun() -> setup_connecion(#{safe => true}) end,
