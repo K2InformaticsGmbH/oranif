19eaae878ac8d9138647cae78c9db2fe5c5c67c9
Bikram Chatterjee <chatterjee@bluewin.ch>
2019-07-25 00:35:32 +0200

    implementation without test coverage

--- c_src/dpiStmt_nif.h
+++ c_src/dpiStmt_nif.h
@@ -27,6 +27,7 @@ extern DPI_NIF_FUN(stmt_bindValueByPos);
 extern DPI_NIF_FUN(stmt_define);
 extern DPI_NIF_FUN(stmt_defineValue);
 extern DPI_NIF_FUN(stmt_execute);
+extern DPI_NIF_FUN(stmt_executeMany);
 extern DPI_NIF_FUN(stmt_fetch);
 extern DPI_NIF_FUN(stmt_getQueryInfo);
 extern DPI_NIF_FUN(stmt_getQueryValue);
@@ -42,6 +43,7 @@ extern DPI_NIF_FUN(stmt_getInfo);
         IOB_NIF(stmt_define, 3),             \
         IOB_NIF(stmt_defineValue, 7),        \
         IOB_NIF(stmt_execute, 2),            \
+        IOB_NIF(stmt_executeMany, 3),        \
         IOB_NIF(stmt_fetch, 1),              \
         IOB_NIF(stmt_getQueryInfo, 2),       \
         IOB_NIF(stmt_getQueryValue, 2),      \
