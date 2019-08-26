c322e9432bf9a36c8119aa6ab52b8494efb97d74
Bikram Chatterjee <chatterjee@bluewin.ch>
2019-08-26 15:48:18 +0200

    run moved to test

--- test/run.sh
+++ test/run.sh
@@ -3,4 +3,5 @@ rm -rf cov_html/ *.gc* *.info
 rebar3 do clean, compile, eunit
 lcov --directory . --capture --output-file coverage.info
 lcov -r coverage.info "*.h" -o coverage.info
+genhtml coverage.info --output-directory cov_html
 lcov --list coverage.info
